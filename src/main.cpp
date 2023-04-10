c02df0c947e789274045f62b92abd9c509783fe1
Adit Chauhan <chauhan.adit.98@gmail.com>
2023-04-10 15:12:12 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,6 @@
 #include <spdlog/spdlog.h>
 #include <unistd.h>
 
-// int main() {
-//   // INIT pigpio;;
-//   // int cfg = gpioCfgGetInternals();
-//   // cfg |= PI_CFG_NOSIGHANDLER;
-//   // gpioCfgSetInternals(cfg);
-//   if (gpioInitialise() < 0) {
-//     spdlog::error("Failed to Init pigs");
-//     return 1;
-//   }
-//   LunaDriver driver = LunaDriver();
-//
-//   while (1) {
-//     sleep(5);
-//   }
-//
-//   return 0;
-// }
-//
 int main() {
   // Initialize pigpio library
   if (gpioInitialise() < 0) {
@@ -34,8 +16,8 @@ int main() {
   int address = 0x10;
 
   // Set register addresses for distance data
-  int distance_high_byte = 0x00;
-  int distance_low_byte = 0x01;
+  int distance_high_byte = 0x01;
+  int distance_low_byte = 0x00;
 
   // Open I2C bus
   int handle = i2cOpen(1, address, 0);
@@ -46,22 +28,26 @@ int main() {
   }
 
   // Read distance data from sensor
+  int current_mode = 10;
+  current_mode = i2cReadByteData(handle, luna::reg::Mode);
+  spdlog::info("Current Mode");
+  if (i2cWriteByteData(handle, luna::reg::Mode, luna::settings::Mode::Trigger) <
+      0) {
+    spdlog::error("failed to change luna mode");
+  }
+
   while (true) {
     // Write to sensor to trigger measurement
-    char txbuf[1] = {0x01};
-    if (i2cWriteByteData(handle, 0x01, 1) < 0) {
+    if (i2cWriteByteData(handle, luna::reg::TrigShot, 0x01) < 0) {
       spdlog::error("Failed to write to sensor");
       i2cClose(handle);
       gpioTerminate();
       return 1;
     }
-
-    // Wait for measurement to complete
-    usleep(20000);
-
+    usleep(2000);
     // Read distance data from sensor
-    int high_byte = i2cReadByteData(handle, distance_high_byte);
-    int low_byte = i2cReadByteData(handle, distance_low_byte);
+    int high_byte = i2cReadByteData(handle, luna::reg::DistHigh);
+    int low_byte = i2cReadByteData(handle, luna::reg::DistLow);
     if (high_byte < 0 || low_byte < 0) {
       spdlog::error("Failed to read from sensor");
       i2cClose(handle);
