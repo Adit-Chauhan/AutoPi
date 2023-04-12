8388dc26fe2a0e5f41e2a88e499509e3236b82fe
Adit Chauhan <chauhan.adit.98@gmail.com>
2023-04-12 16:25:25 +0100

    test 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,86 @@
-#include "utils/server.h"
-#include <spdlog/spdlog.h>
-#include <string>
-class HelloResp : public serverCallback {
-  void serverAction() { spdlog::info("Running Hello Resp"); }
-};
-
-class DateResp : public serverCallback {
-  void serverAction() { spdlog::info("Running Date Resp"); }
-};
-
-#include <cmath>
+//#include "utils/server.h"
+//#include <spdlog/spdlog.h>
+//#include <string>
+// class HelloResp : public serverCallback {
+//  void serverAction() { spdlog::info("Running Hello Resp"); }
+//};
+//
+// class DateResp : public serverCallback {
+//  void serverAction() { spdlog::info("Running Date Resp"); }
+//};
 #include <iostream>
-#include <serial/serial.h>
+#include <libserial/SerialPort.h>
+#include <libserial/SerialPortConstants.h>
+#include <libserial/SerialStream.h>
+#include <string>
+#include <unistd.h>
 
 using namespace std;
+using namespace LibSerial;
 
-serial::Serial ser("/dev/serial0", 115200, serial::Timeout::simpleTimeout(0));
-
-void read_tfluna_data(double &distance, double &strength, double &temperature) {
+void read_tfluna_data(SerialPort &ser, double &distance, double &strength,
+                      double &temperature) {
   while (true) {
-    size_t counter = ser.available();
-    if (counter > 8) {
-      uint8_t bytes_serial[9];
-      ser.read(bytes_serial, 9);
-      ser.flushInput();
-
-      if (bytes_serial[0] == 0x59 && bytes_serial[1] == 0x59) {
-        distance = bytes_serial[2] + bytes_serial[3] * 256;
-        strength = bytes_serial[4] + bytes_serial[5] * 256;
-        temperature = bytes_serial[6] + bytes_serial[7] * 256;
-        temperature = (temperature / 8.0) - 256.0;
-        distance /= 100.0;
-        return;
-      }
+    while (!ser.IsDataAvailable()) {
+      usleep(1000);
     }
+    std::string dataString;
+    ser.ReadLine(dataString);
+    cout << dataString << '\n';
+    //    if (bytes_serial[0] == 0x59 && bytes_serial[1] == 0x59) {
+    //      distance = bytes_serial[2] + bytes_serial[3] * 256;
+    //      strength = bytes_serial[4] + bytes_serial[5] * 256;
+    //      temperature = bytes_serial[6] + bytes_serial[7] * 256;
+    //      temperature = (temperature / 8.0) - 256.0;
+    //      distance /= 100.0;
+    return;
   }
 }
 
 int main() {
-  if (!ser.isOpen()) {
-    ser.open();
-  }
+  SerialPort ser;
+  ser.Open("/dev/serial0");
+
+  ser.SetBaudRate(BaudRate::BAUD_115200);
+  ser.SetCharacterSize(CharacterSize::CHAR_SIZE_8);
+  ser.SetStopBits(StopBits::STOP_BITS_1);
 
   double distance, strength, temperature;
-  read_tfluna_data(distance, strength, temperature);
+  read_tfluna_data(ser, distance, strength, temperature);
 
-  cout << "Distance: " << fixed << setprecision(2) << distance
-       << " m, Strength: " << fixed << setprecision(0) << strength
-       << " / 65535 (16-bit), Chip Temperature: " << fixed << setprecision(1)
-       << temperature << " C" << endl;
+  //  cout << "Distance: " << fixed << setprecision(2) << distance
+  //       << " m, Strength: " << fixed << setprecision(0) << strength
+  //       << " / 65535 (16-bit), Chip Temperature: " << fixed <<
+  //       setprecision(1)
+  //       << temperature << " C" << endl;
 
-  ser.close();
   return 0;
 }
-
 // int main() {
-//   spdlog::info("Hello Dev");
+//   // Send data
+//   string tx_buffer = "Hello, world!";
+//   serial_port << tx_buffer << endl;
 //
-//   HelloResp hello;
-//   DateResp date;
-//   Server server = Server();
+//   // Receive data
+//   string rx_buffer;
+//   serial_port >> rx_buffer;
+//   if (serial_port.fail()) {
+//     cout << "Error - Unable to receive data." << endl;
+//     return 1;
+//   }
+//   cout << "Received " << rx_buffer.length() << " bytes: " << rx_buffer <<
+//   endl;
 //
-//   server.register_callback_action("/hello", &hello);
-//   server.register_callback_action("/date", &date);
-//   server.run();
 //   return 0;
-// }
+// } // int main() {
+//    spdlog::info("Hello Dev");
+//
+//    HelloResp hello;
+//    DateResp date;
+//    Server server = Server();
+//
+//    server.register_callback_action("/hello", &hello);
+//    server.register_callback_action("/date", &date);
+//    server.run();
+//    return 0;
+//  }
