031704a1a9337e68fc2154706777785c443a4b40
rem2702 <reshmi2722000@gmail.com>
2023-04-13 09:12:37 +0100

    change infos

--- src/email.h
+++ src/email.h
@@ -6,50 +6,81 @@
 #define AUTOPI_EMAIL_H
 
 #include <iostream>
-#include <thread>
-#include <SMTPClient.h>
-#include "alcohol_detection.h" // header file for alcohol detection function
-#include "drowsiness_detection.h" // header file for drowsiness detection function
+#include <string>
+#include <functional>
+#include <unistd.h>
+#include "SMTPclient.h"
 
-class Detection {
+class EmailSender {
 public:
-    Detection() {} // default constructor
+    // Constructor
+    EmailSender(const std::string& username, const std::string& password) :
+            m_username(username),
+            m_password(password) {}
 
-    void checkAlcoholLevel() {
-        float level = getAlcoholLevel(); // call alcohol detection function
-        if (level > 0.08) {
-            std::cout << "You are legally intoxicated." << std::endl;
-            sendEmail("Intoxication Alert", "The person is legally intoxicated."); // call send email function
-        } else {
-            std::cout << "You are not legally intoxicated." << std::endl;
+    // Setter for callback function
+    void setCallback(std::function<void()> callback) {
+        m_callback = callback;
+    }
+
+    // Function to send email
+    void sendEmail(const std::string& to, const std::string& subject, const std::string& body) {
+        SMTPclient smtp;
+        smtp.server = "smtp.gmail.com";
+        smtp.port = 587;
+        smtp.login = m_username;
+        smtp.password = m_password;
+
+        // Set email parameters
+        smtp.from = m_username;
+        smtp.to = to;
+        smtp.subject = subject;
+        smtp.message = body;
+
+        // Send email
+        if (smtp.send()) {
+            std::cout << "Email sent successfully!" << std::endl;
+
+            // Call callback function
+            if (m_callback) {
+                m_callback();
+            }
+        }
+        else {
+            std::cout << "Error sending email." << std::endl;
         }
     }
 
-    void checkDrowsiness() {
-        bool isDrowsy = detectDrowsiness(); // call drowsiness detection function
-        if (isDrowsy) {
-            std::cout << "You appear to be drowsy." << std::endl;
-            sendEmail("Drowsiness Alert", "The person appears to be drowsy."); // call send email function
-        } else {
-            std::cout << "You do not appear to be drowsy." << std::endl;
-        }
-    }
+private:
+    // SMTP login credentials
+    std::string m_username;
+    std::string m_password;
 
-    void sendEmail(const std::string& subject, const std::string& body) {
-        // code for sending email
-        std::cout << "Sending email: " << subject << std::endl;
-        SMTPClient client("smtp.gmail.com", 587, SMTPClient::TLS);
-        client.login("your_email@gmail.com", "your_password");
-        client.send("your_email@gmail.com", "friend_email@gmail.com", subject, body);
-        client.quit();
-        std::cout << "Email sent to friend_email@gmail.com" << std::endl;
-    }
+    // Callback function
+    std::function<void()> m_callback;
 };
 
-class email {
+// Function to be called by the callback
+void myCallback() {
+    std::cout << "Email sent. Taking appropriate action..." << std::endl;
+}
 
+int main() {
+    // Create instance of EmailSender with login credentials
+    EmailSender sender("aknair174@gmail.com", "aknair@1");
 
-};
+    // Set the callback function
+    sender.setCallback(myCallback);
+
+    // Simulate alcohol detection and drowsiness
+    std::cout << "Detecting alcohol and drowsiness..." << std::endl;
+
+    // Send email with parameters
+    sender.sendEmail("reshmi2722000@gmail.com", "Alert", "Email body");
+
+    return 0;
+}
+
 
 
 #endif //AUTOPI_EMAIL_H
