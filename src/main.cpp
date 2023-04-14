161d69cdbcdddfc8634d16874a56d0f2e338b90c
namithamaria <namithamariabenny28@gmail.com>
2023-04-14 21:07:25 +0530

    Modification with video capture

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <libcamera/libcamera.h>
+#include <libcamera/stream.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -27,13 +29,7 @@ public:
           face_cascade_("data/haarcascade_frontalface_default.xml"),
           eye_cascade_("data/haarcascade_eye.xml") {}
 
-    bool detect_drowsiness() {
-        cv::Mat frame;
-        if (!read_frame_from_stdin(frame)) {
-            std::cerr << "Failed to read frame from stdin" << std::endl;
-            return false;
-        }
-
+    bool detect_drowsiness(cv::Mat& frame) {
         // Convert the frame to grayscale
         cv::Mat gray;
         cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
@@ -63,21 +59,6 @@ public:
     }
 
 private:
-    bool read_frame_from_stdin(cv::Mat& frame) {
-        int rows, cols, type;
-        std::cin.read(reinterpret_cast<char*>(&rows), sizeof(int));
-        std::cin.read(reinterpret_cast<char*>(&cols), sizeof(int));
-        std::cin.read(reinterpret_cast<char*>(&type), sizeof(int));
-
-        if (!std::cin) {
-            return false;
-        }
-
-        frame.create(rows, cols, type);
-        std::cin.read(reinterpret_cast<char*>(frame.data), frame.total() * frame.elemSize());
-        return !!std::cin;
-    }
-
     Driver& driver_;
     cv::CascadeClassifier face_cascade_;
     cv::CascadeClassifier eye_cascade_;
@@ -87,14 +68,70 @@ int main() {
     Driver driver;
     DrowsinessDetector detector(driver);
 
+    libcamera::CameraManager cm;
+
+    if (cm.start() != 0) {
+        std::cerr << "Failed to start the camera manager" << std::endl;
+        return -1;
+    }
+
+    std::shared_ptr<libcamera::Camera> camera = cm.getCamera(0);
+    if (!camera) {
+        std::cerr << "Failed to get the camera" << std::endl;
+        return -1;
+    }
+
+    camera->requestStart();
+
     while (true) {
-        bool drowsy = detector.detect_drowsiness();
+        libcamera::Request *request = camera->createRequest();
+        if (!request) {
+            std::cerr << "Failed to create request" << std::endl;
+            break;
+        }
+
+        libcamera::Stream *stream = camera->stream();
+        request->addBuffer(stream);
+
+        if (camera->queueRequest(request) != 0) {
+            std::cerr << "Failed to queue request" << std::endl;
+            break;
+        }
+
+        const libcamera::FrameBuffer *buffer = stream->getBuffer();
+                if (!buffer) {
+            std::cerr << "Failed to get buffer" << std::endl;
+            break;
+        }
+
+        // Get the buffer data and create an OpenCV Mat
+        const libcamera::FrameBufferPlane &plane = buffer->planes().front();
+        void *data = mmap(NULL, plane.length, PROT_READ, MAP_SHARED, plane.fd, 0);
+        if (data == MAP_FAILED) {
+            std::cerr << "Failed to mmap buffer" << std::endl;
+            break;
+        }
+
+        cv::Mat img(stream->configuration().pixelFormat.size.height,
+                    stream->configuration().pixelFormat.size.width,
+                    CV_8UC3,
+                    data,
+                    plane.stride);
+
+        bool drowsy = detector.detect_drowsiness(img);
         driver.set_drowsy(drowsy);
 
         if (driver.is_drowsy()) {
             std::cout << "Driver is drowsy!" << std::endl;
         }
+
+        munmap(data, plane.length);
+        stream->putBuffer(buffer);
     }
+
+    camera->requestStop();
+
+    cm.stop();
 
     return 0;
 }
