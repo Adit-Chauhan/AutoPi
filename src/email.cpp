fcb7a36be2216cc2ef260a82d34e57c82f418058
rem2702 <reshmi2722000@gmail.com>
2023-04-16 16:12:41 +0100

    changed email code

--- src/email.cpp
+++ src/email.cpp
@@ -1,5 +1,55 @@
-//
-// Created by Lenovo on 12-04-2023.
-//
-
 #include "email.h"
+
+EmailSender::EmailSender(const std::string& username, const std::string& password) :
+        m_username(username),
+        m_password(password) {}
+
+void EmailSender::sendEmails(const std::string& subject, const std::string& body) {
+    SMTPclient smtp;
+    smtp.server = "smtp.gmail.com";
+    smtp.port = 587;
+    smtp.login = m_username;
+    smtp.password = m_password;
+
+    // Set email parameters
+    for (auto to:recivers){
+        smtp.from = m_username;
+        smtp.to = to;
+        smtp.subject = subject;
+        smtp.message = body;
+
+        // Send email
+        if (smtp.send()) {
+            std::cout << "Email sent successfully!" << std::endl;
+        } else {
+            std::cout << "Error sending email." << std::endl;
+        }
+    }
+}
+
+void EmailSender::new_friend(std::string newEmail){
+    recivers.push_back(newEmail);
+}
+
+DrunkCallback::DrunkCallback(EmailSender* email) :
+        m_sender(email){}
+
+void DrunkCallback::send_email() {
+    // Send email to friend
+    std::string subject = "Driver is drunk";
+    std::string body = "Please take appropriate action.";
+    m_sender.sendEmails(subject, body);
+
+    std::cout << "Drunk email sent to friend." << std::endl;
+}
+
+SleepyCallback::SleepyCallback(EmailSender *email) :
+        m_sender(email) {}
+
+void SleepyCallback::send_email() {
+    // Send email to friend
+    std::string subject = "Driver is drowsy";
+    std::string body = "Please take appropriate action.";
+    m_sender.sendEmails(subject, body);
+    std::cout << "Drowsy email sent to friend." << std::endl;
+}
--- src/email.h
+++ src/email.h
@@ -1,7 +1,3 @@
-//
-// Created by Lenovo on 12-04-2023.
-//
-
 #ifndef AUTOPI_EMAIL_H
 #define AUTOPI_EMAIL_H
 
@@ -14,36 +10,13 @@
 class EmailSender {
 public:
     // Constructor
-    EmailSender(const std::string& username, const std::string& password) :
-            m_username(username),
-            m_password(password) {}
+    EmailSender(const std::string& username, const std::string& password);
 
     // Function to send email
-    void sendEmails(const std::string& subject, const std::string& body) {
-        SMTPclient smtp;
-        smtp.server = "smtp.gmail.com";
-        smtp.port = 587;
-        smtp.login = m_username;
-        smtp.password = m_password;
+    void sendEmails(const std::string& subject, const std::string& body);
 
-        // Set email parameters
-        for (auto to:recivers){
-            smtp.from = m_username;
-            smtp.to = to;
-            smtp.subject = subject;
-            smtp.message = body;
+    void new_friend(std::string newEmail);
 
-            // Send email
-            if (smtp.send()) {
-                std::cout << "Email sent successfully!" << std::endl;
-            } else {
-                std::cout << "Error sending email." << std::endl;
-            }
-        }
-    }
-    void new_friend(std::string newEmail){
-        recivers.pushback(newEmail);
-    }
 private:
     // SMTP login credentials
     std::string m_username;
@@ -63,31 +36,18 @@ protected:
 
 class DrunkCallback : public emailCallback {
 public:
-    DrunkCallback(EmailSender* email) :
-            m_sender(email){}
+    DrunkCallback(EmailSender* email);
 
-    void send_email() override {
-        // Send email to friend
-        std::string subject = "Driver is drunk";
-        std::string body = "Please take appropriate action.";
-        m_sender.sendEmails(subject, body);
+    void send_email() override;
 
-        std::cout << "Drunk email sent to friend." << std::endl;
-    }
 };
 
 class SleepyCallback : public emailCallback {
 public:
-    SleepyCallback(EmailSender *email):
-            m_sender(email) {}
+    SleepyCallback(EmailSender *email);
 
-    void send_email() override {
-        // Send email to friend
-        std::string subject = "Driver is drowsy";
-        std::string body = "Please take appropriate action.";
-        m_sender.sendEmails(subject, body);
-        std::cout << "Drowsy email sent to friend." << std::endl;
-    }
+    void send_email() override;
 
 };
+
 #endif //AUTOPI_EMAIL_H
