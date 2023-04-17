3843436ca604e80d7ae42e177162db159cb05abb
Adit Chauhan <chauhan.adit.98@gmail.com>
2023-04-18 00:39:41 +0100

    extra ;

--- src/inits/mq3_init.cpp
+++ src/inits/mq3_init.cpp
@@ -1,6 +1,5 @@
 #include "mq3_init.h"
-std::unique_ptr<mq3Driver> make_mq3(std::shared_ptr<EmailSender> e);
-{
+std::unique_ptr<mq3Driver> make_mq3(std::shared_ptr<EmailSender> e) {
   auto driver = std::make_unique<mq3Driver>();
   auto drunk = std::make_unique<isDrunk>(std::make_unique<DrunkEmail>(e));
   driver->registerCallback(move(drunk));
