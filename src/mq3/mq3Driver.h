a53bf0117cb969f2e467d962e6e1fb9c51bcd516
Adit Chauhan <chauhan.adit.98@gmail.com>
2023-04-17 10:41:41 +0100

    Mq3 init

--- src/mq3/mq3Driver.h
+++ src/mq3/mq3Driver.h
@@ -5,36 +5,34 @@
 #ifndef AUTOPI_MQ3DRIVER_H
 #define AUTOPI_MQ3DRIVER_H
 #include "mq3sensor.h"
-class mq3Callback{
+#include <memory>
+class mq3Callback {
 public:
-    virtual void hasSample(float sample) = 0;
+  virtual void hasSample(float sample) = 0;
 };
 
 class mq3Driver {
 public:
-    mq3Driver(){
-        sensor = new MQ3Sensor(11, 9, 10, 8, 4);
-        sensor->read_sensor(); // First Value is always zero
-        sensor->dataReady = false;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        sensor->read_sensor(); // actual Value
-    }
-    void dataReady(){
-        sensor->dataReady = false;
-        sensor->read_sensor();
-        callback->hasSample(sensor->get_sensor_ppm());
-    }
-    void registerCallback(mq3Callback* fn){
-        callback = fn;
-    }
-    ~mq3Driver(){
-        delete sensor;
-    }
+  mq3Driver() {
+    sensor = std::make_unique<MQ3Sensor>(11, 9, 10, 8, 4);
+    sensor->read_sensor(); // First Value is always zero
+    sensor->dataReady = false;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    sensor->read_sensor(); // actual Value
+  }
+  void dataReady() {
+    sensor->dataReady = false;
+    sensor->read_sensor();
+    callback->hasSample(sensor->get_sensor_ppm());
+  }
+  void registerCallback(std::unique_ptr<mq3Callback> fn) {
+    callback = move(fn);
+  }
+  ~mq3Driver() {}
 
 private:
-    MQ3Sensor* sensor;
-    mq3Callback* callback;
+  std::unique_ptr<MQ3Sensor> sensor;
+  std::unique_ptr<mq3Callback> callback;
 };
 
-
-#endif //AUTOPI_MQ3DRIVER_H
+#endif // AUTOPI_MQ3DRIVER_H
