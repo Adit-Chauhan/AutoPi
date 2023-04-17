53ac2b362a800a363c2b351a8995335a4c31e573
Adit Chauhan <chauhan.adit.98@gmail.com>
2023-04-18 00:38:20 +0100

    fix bug

---
+++ src/inits/mq3_init.cpp
@@ -0,0 +1,8 @@
+#include "mq3_init.h"
+std::unique_ptr<mq3Driver> make_mq3(std::shared_ptr<EmailSender> e);
+{
+  auto driver = std::make_unique<mq3Driver>();
+  auto drunk = std::make_unique<isDrunk>(std::make_unique<DrunkEmail>(e));
+  driver->registerCallback(move(drunk));
+  return driver;
+}
--- src/inits/mq3_init.h
+++ src/inits/mq3_init.h
@@ -77,11 +77,6 @@ public:
  * @param e A shared pointer to EmailSender class to send email
  * @return A unique pointer to mq3Driver class instance
  */
-std::unique_ptr<mq3Driver> make_mq3(std::shared_ptr<EmailSender> e) {
-  auto driver = std::make_unique<mq3Driver>();
-  auto drunk = std::make_unique<isDrunk>(std::make_unique<DrunkEmail>(e));
-  driver->registerCallback(move(drunk));
-  return driver;
-}
+std::unique_ptr<mq3Driver> make_mq3(std::shared_ptr<EmailSender> e);
 
 #endif // MQ3_INIT_H_
