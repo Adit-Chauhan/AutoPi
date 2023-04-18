94e21a784b2eeb21127e411861fc480a0c7690fa
Adit Chauhan <chauhan.adit.98@gmail.com>
2023-04-18 17:22:51 +0100

    rollback

--- src/lidar/lunadriver.cpp
+++ src/lidar/lunadriver.cpp
@@ -4,8 +4,6 @@
 #include <algorithm>
 #include <array>
 #include <cstdint>
-#include <fmt/format.h>
-#include <memory>
 #include <pigpio.h>
 #include <poll.h>
 #include <spdlog/spdlog.h>
@@ -22,26 +20,29 @@ void LunaDriver::dataReady() {
   if (callback == NULL) {
     return;
   }
-  callback->hasSample(normal_read_buffer);
-  spdlog::trace("exiting Data ready");
+  callback->hasSample(normal_read_buffer.data());
 }
 
 void LunaDriver::read_thread() {
-  // Cause a mem leak
-  pollfd *p_fd = new pollfd;
-  p_fd->fd = lidar.get_raw_fd();
-  p_fd->events = POLLIN;
-  p_fd->revents = 0;
-  spdlog::info("Pollfd:: ptr = {}, fd = {}, event = {}, revents = {}",
-               fmt::ptr(p_fd), p_fd->fd, p_fd->events, p_fd->revents);
+  struct pollfd p_fd;
+  p_fd.fd = lidar.get_raw_fd();
+  p_fd.events = POLLIN;
+  p_fd.revents = 0;
   while (true) {
-    spdlog::trace("start of loop");
-    if (check_data_type(p_fd)) {
+    if (check_data_type(&p_fd)) {
       // Flush If improper data
       spdlog::error("Improper lidar Data");
+#ifdef DBG_SLEEP
+      spdlog::error("Improper lidar Data:: {:x}  {:x}", normal_read_buffer[0],
+                    normal_read_buffer[1]);
+      sleep(1);
+      reset_luna();
+#endif
       std::exit(42);
     }
-    normal_data(p_fd);
+    normal_data(&p_fd);
+    //   wait_for_data(&p_fd, 9);
+    // lidar.read(normal_read_buffer.data(), 9);
     dataReady();
   }
 }
@@ -49,10 +50,8 @@ void LunaDriver::read_thread() {
 int LunaDriver::check_data_type(pollfd *p_fd) {
   spdlog::debug("Checking Data type");
   wait_for_data(p_fd, 2);
-  spdlog::trace("waited for data");
   std::array<uint8_t, 2> arr;
   lidar.read(arr.data(), 2);
-  spdlog::trace("print data");
   if (arr[0] == 0x59 && arr[1] == 0x59) {
     spdlog::debug("Found Normal Data");
     return 0; // Normal Data
@@ -62,6 +61,10 @@ int LunaDriver::check_data_type(pollfd *p_fd) {
     return arr[1]; // Return next read size
   }
   spdlog::debug("Found error");
+#ifdef DBG_SLEEP
+  normal_read_buffer[0] = arr[0];
+  normal_read_buffer[1] = arr[1];
+#endif       // DBG_SLEEP
   return -1; // Error
 }
 
@@ -75,31 +78,26 @@ void LunaDriver::registerCallback(std::unique_ptr<LunaCallback> fn) {
 
 void LunaDriver::wait_for_data(pollfd *p_fd, uint8_t num_bytes) {
   int bytes_available;
-  spdlog::info(
-      "Pollfd before wait:: ptr = {}, fd = {}, event = {}, revents = {}",
-      fmt::ptr(p_fd), p_fd->fd, p_fd->events, p_fd->revents);
-
   while (bytes_available < num_bytes) {
-    spdlog::trace("Runing poll");
     int check = poll(p_fd, 1, -1);
     if (check != 1 || p_fd->revents == POLLERR || p_fd->revents == POLLNVAL) {
       spdlog::error("Error in polling fd");
     }
     ioctl(p_fd->fd, FIONREAD, &bytes_available);
     spdlog::trace("Number of bytes {}", bytes_available);
-    spdlog::info(
-        "Pollfd during wait:: ptr = {}, fd = {}, event = {}, revents = {}",
-        fmt::ptr(p_fd), p_fd->fd, p_fd->events, p_fd->revents);
   }
+  return;
 }
 
 void LunaDriver::normal_data(pollfd *p_fd) {
   wait_for_data(p_fd, 7);
   uint8_t data[7];
   lidar.read(data, 7);
-  normal_read_buffer[0] = 0x59;
-  normal_read_buffer[1] = 0x59;
-  std::copy(data, data + 7, normal_read_buffer + 2);
+  normal_read_buffer = {
+      0x59,
+      0x59,
+  };
+  std::copy(data, data + 7, normal_read_buffer.begin() + 2);
   is_normal_data = true;
 }
 
